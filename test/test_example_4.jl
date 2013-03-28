a29488f5d83ab39d92ae636dd62a0a8c82917346
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-03-28 09:26:43 +0100

    Added example 4 from SimPy

---
+++ test/test_example_4.jl
@@ -0,0 +1,31 @@
+require("../src/SimJulia.jl")
+using SimJulia
+
+function buy(customer::Process, budget::Float64)
+	println("$(now(customer)) $customer: Here I am at the shops, I have $budget to spend")
+	t = 5.0
+	for i = 1:4
+		if budget < 10.0
+			break
+		end
+		produce(hold(customer, t))
+		println("$(now(customer)) $customer: I just bought something")
+		budget -= 10.0
+	end
+	println("$(now(customer)) $customer: All I have left is $budget, I am going home")
+end
+
+function execute(source::Process, finish::Float64)
+	i = 1
+	while now(source) < finish
+		c = Process(source.simulation, "Customer$(i)")
+		activate(c, now(source), buy, 100.0*rand())
+		produce(hold(source, 10.0))
+		i = i+1
+	end
+end
+
+sim = Simulation(uint(16))
+g = Process(sim, "Source")
+activate(g, 0.0, execute, 30.0)
+run(sim, 100.0)
