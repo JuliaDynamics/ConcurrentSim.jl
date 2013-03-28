0686cc1beb9cd9fd729bce3078deebd9ac04d980
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-03-28 09:07:27 +0100

    Added example 2 from SimPy

---
+++ test/test_example_2.jl
@@ -0,0 +1,18 @@
+require("../src/SimJulia.jl")
+using SimJulia
+
+function buy(customer::Process, budget::Float64)
+	println("$customer: Here I am at the shops")
+	t = 5.0
+	for i = 1:4
+		produce(hold(customer, t))
+		println("$customer: I just bought something")
+		budget -= 10.0
+	end
+	println("$customer: All I have left is $budget, I am going home")
+end
+
+sim = Simulation(uint(16))
+c = Process(sim, "Evelyn")
+activate(c, 10.0, buy, 100.0)
+run(sim, 100.0)
