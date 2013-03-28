e0c2b7152003808a829cf0816db4bdea071fa5c2
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-03-28 09:32:07 +0100

    Cleanup output of example 4

--- test/test_example_4.jl
+++ test/test_example_4.jl
@@ -2,7 +2,7 @@ require("../src/SimJulia.jl")
 using SimJulia
 
 function buy(customer::Process, budget::Float64)
-	println("$(now(customer)) $customer: Here I am at the shops, I have $budget to spend")
+	println("$(now(customer)) $customer: Here I am at the shops, I have $(round(budget)) to spend")
 	t = 5.0
 	for i = 1:4
 		if budget < 10.0
@@ -12,14 +12,14 @@ function buy(customer::Process, budget::Float64)
 		println("$(now(customer)) $customer: I just bought something")
 		budget -= 10.0
 	end
-	println("$(now(customer)) $customer: All I have left is $budget, I am going home")
+	println("$(now(customer)) $customer: All I have left is $(round(budget)), I am going home")
 end
 
 function execute(source::Process, finish::Float64)
 	i = 1
 	while now(source) < finish
 		c = Process(source.simulation, "Customer$(i)")
-		activate(c, now(source), buy, 100.0*rand())
+		activate(c, now(source), buy, round(100.0*rand()))
 		produce(hold(source, 10.0))
 		i = i+1
 	end
