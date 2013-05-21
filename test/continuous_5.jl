c9faf808cbe12fa01fa4ffd108dbf4f6323a2025
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-05-21 10:17:24 +0200

    Fixed bug in continuous_5.jl

--- test/continuous_5.jl
+++ test/continuous_5.jl
@@ -7,9 +7,8 @@ const height = 0.046
 const nbr_stones = 55
 const km = m * g * 0.5 * sqrt(width^2 + height^2)
 const theta = m * (width^2 + height^2) / 3.0
-const initial_omega = height / 2.0 * 1.0e-5 / theta
 
-function dynamics(time::Float64, variables::Vector{Variable})
+function dynamics(time::Float64, variables::Vector{Variable}, nr::Int)
 	omega = variables[1]
 	phi = variables[2]
 	omega.rate = km * sin(phi.state) / theta
@@ -19,7 +18,8 @@ end
 function stone(process::Process, nr::Int, distance::Float64, initial_omega::Float64, phi_push::Float64, k0::Float64)
 	phi = Variable(0.0)
 	omega = Variable(initial_omega)
-	start(simulation(process), [omega, phi], dynamics)
+	f = (time::Float64, variables::Vector{Variable}) -> dynamics(time, variables, nr)
+	start(simulation(process), [omega, phi], f)
 	if nr < nbr_stones
 		waituntil(process, ()->return phi.state >= phi_push)
 		new_stone = Process(simulation(process), "Stone $nr")
@@ -27,16 +27,17 @@ function stone(process::Process, nr::Int, distance::Float64, initial_omega::Floa
 		omega.state = (1.0 - sqrt(k0)) * omega.state
 	end
 	waituntil(process, ()->return phi.state >= pi/2.0)
-	stop(simulation(process), [omega, phi], dynamics)
+	stop(simulation(process), [omega, phi], f)
 end
 
 function game(distance::Float64)
 	sim = Simulation(uint(32), 1.0e-6, 0.2, 1.0e-4, 1.0e-4)
 	domino = Process(sim, "Stone 1")
 	k0 = 1.0 - (distance - width)^2 / height^2
+	initial_omega = height / 2.0 * 1.0e-5 / theta
 	phi_push = asin((distance - width) / height)
 	activate(domino, 0.0, stone, 1, distance, initial_omega, phi_push, k0)
-	run(sim, 10.0)
+	run(sim, 100.0)
 	velocity = distance * (nbr_stones - 1) / now(domino)
 	return velocity
 end
