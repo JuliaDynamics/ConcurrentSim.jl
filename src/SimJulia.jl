8228c7a7a0d41d4319a14e0517e99cfb79e68aab
Ben Lauwens <ben.lauwens@gmail.com>
2015-02-25 11:52:14 +0100

    Initial branch for new version

--- src/SimJulia.jl
+++ src/SimJulia.jl
@@ -1,23 +1,3 @@
 module SimJulia
-	importall Base
-	export Simulation, Variable, Process, Signal, Resource, Monitor, Level, Store
-	export run, run_continuous, stop, register, reset
-	export observe, count, mean, var, time_average, tseries, yseries, histogram, report
-	export now, simulation, terminated, active, passive, interrupted
-	export activate, reactivate, interrupt, interrupt_reset, interrupt_left, interrupt_cause
-	export sleep, hold, waituntil
-	export fire, wait, queue, param
-	export state, rate
-	export occupied, request, release, acquired, wait_monitor, activity_monitor
-	export amount, buffer, put, get, got, put_monitor, get_monitor, buffer_monitor
-	include("priority_queue.jl")
-	include("events.jl")
-	include("monitors.jl")
-	include("variables.jl")
-	include("simulations.jl")
-	include("processes.jl")
-	include("signals.jl")
-	include("resources.jl")
-	include("levels.jl")
-	include("stores.jl")
+
 end
