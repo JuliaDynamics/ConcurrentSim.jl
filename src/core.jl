acb86188b1d29980c46cabe98c3e222d5dffe5ab
= <=>
2015-03-03 21:01:23 +0100

    Make the PriorityQueue work on julia v0.3 and v0.4

--- src/core.jl
+++ src/core.jl
@@ -2,13 +2,13 @@ using Base.Collections
 
 type Environment
   now :: Float64
-  heap :: PriorityQueue{Event, EventID, Base.Order.ForwardOrdering}
+  heap :: PriorityQueue
   eid :: Uint16
   active_proc :: Process
   function Environment(initial_time::Float64=0.0)
     env = new()
     env.now = initial_time
-    env.heap = PriorityQueue{Event, EventID, Base.Order.ForwardOrdering}()
+    env.heap = PriorityQueue()
     env.eid = 0
     return env
   end
