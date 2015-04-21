f407c515d270dd1f28966f00a614ede625041564
BenLauwens <=>
2015-04-21 16:43:16 +0200

    Problem with PriorityQueue in Julia v0.4

--- src/core.jl
+++ src/core.jl
@@ -35,13 +35,13 @@ end
 
 type Environment
   now :: Float64
-  heap :: PriorityQueue{Event, EventID}
+  heap :: PriorityQueue
   eid :: Uint16
   active_proc :: Process
   function Environment(initial_time::Float64=0.0)
     env = new()
     env.now = initial_time
-    env.heap = PriorityQueue{Event, EventID}()
+    env.heap = PriorityQueue()
     env.eid = 0
     return env
   end
