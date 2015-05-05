b9ec291a724de6a601b0e8fb99c8017d09944822
Ben Lauwens <ben.lauwens@gmail.com>
2015-05-05 15:54:32 +0200

    Adding example 3 from SimPy 10 min intro

---
+++ examples/simpy_10min_2.jl
@@ -0,0 +1,21 @@
+using SimJulia
+
+function car(env::Environment)
+  while true
+    println("Start parking and charging at $(now(env))")
+    charge_duration = 5.0
+    yield(env, Process(env, charge, charge_duration))
+
+    println("Start driving at $(now(env))")
+    trip_duration = 2.0
+    yield(env, trip_duration)
+  end
+end
+
+function charge(env::Environment, duration::Float64)
+  yield(env, duration)
+end
+
+env = Environment()
+Process(env, car)
+run(env, 15.0)
