d69bcdc631b6ce30cc4117b3dab12ab5a6da5933
BenLauwens <=>
2015-05-07 09:25:08 +0200

    Import base.now for >= Julia v"0.4-"

--- src/SimJulia.jl
+++ src/SimJulia.jl
@@ -1,5 +1,8 @@
 module SimJulia
-  import Base.show, Base.isless, Base.interrupt, Base.yield
+  import Base.show, Base.isless, Base.interrupt, Base.yield, Base.run
+  if VERSION >= v"0.4-"
+    import Base.now
+  end
   export Environment, Event, Timeout, Process, Condition, Interrupt
   export run, succeed, fail, yield
   export triggered, processed
