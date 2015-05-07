d09cec9e735eda6129ab51bcc8bebfc220cbc03d
BenLauwens <=>
2015-05-07 09:18:15 +0200

    Add examples to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,12 @@ for test_file in [
   "test_conditions.jl"]
   include(testpath(test_file))
 end
+
+examplespath(f) = joinpath(Pkg.dir("SimJulia"),"examples",f)
+
+for example_file in [
+  "simpy_welcome.jl",
+  "simpy_10min_1.jl",
+  "simpy_10min_2.jl"]
+  include(examplespath(example_file))
+end
