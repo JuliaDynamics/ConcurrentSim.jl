9cac3759aa4b058d96642dd5db8c63c0e3f735be
Ben Lauwens <ben.lauwens@gmail.com>
2015-05-19 16:30:34 +0200

    include test_resources.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ testpath(f) = joinpath(Pkg.dir("SimJulia"),"test",f)
 for test_file in [
   "test_events.jl",
   "test_processes.jl",
-  "test_conditions.jl"]
+  "test_conditions.jl",
+  "test_resources.jl"]
   include(testpath(test_file))
 end
 
