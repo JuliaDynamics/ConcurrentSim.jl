f192bc8b5674b3e9d514007f14aa36ad71ffc7fb
Ben Lauwens <ben.lauwens@gmail.com>
2015-05-19 16:38:25 +0200

    Small fix

--- test/test_resources.jl
+++ test/test_resources.jl
@@ -17,11 +17,11 @@ function handling(env::Environment, res::Resource, id::Int64)
   yield(Timeout(env, 1.5))
   println("Number $id stops handling at time $(now(env))")
   yield(Release(res))
-  println("Number $id is release at time $(now(env))")
+  println("Number $id is released at time $(now(env))")
 end
 
 env = Environment()
-res = Resource(env)
+res = Resource(env, false)
 Process(env, generator, res)
 run(env, 2.0)
 env2 = Environment(2.0)
