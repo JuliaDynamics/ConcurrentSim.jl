4cdfb7c1128d76871821e1842ca956e579685269
Ben Lauwens <ben.lauwens@gmail.com>
2015-05-20 13:42:57 +0200

    Test preemption

--- test/test_resources.jl
+++ test/test_resources.jl
@@ -2,22 +2,27 @@ using SimJulia
 using Base.Test
 
 function generator(env::Environment, res::Resource)
-  id = 0
+  id = 100
   while true
-    id += 1
+    id -= 1
     yield(Timeout(env, rand()))
     Process(env, handling, res, id)
   end
 end
 
 function handling(env::Environment, res::Resource, id::Int64)
-  println("Number $id requests handling at time $(now(env))")
-  yield(Request(res))
-  println("Number $id starts handling at time $(now(env))")
-  yield(Timeout(env, 1.5))
-  println("Number $id stops handling at time $(now(env))")
-  yield(Release(res))
-  println("Number $id is released at time $(now(env))")
+  try
+    println("Number $id requests handling at time $(now(env))")
+    yield(Request(res, id))
+    println("Number $id starts handling at time $(now(env))")
+    yield(Timeout(env, 1.5))
+    println("Number $id stops handling at time $(now(env))")
+    yield(Release(res))
+    println("Number $id is released at time $(now(env))")
+  catch exc
+    println("Number $id request is interrupted at time $(now(env))")
+  end
+
 end
 
 env = Environment()
@@ -25,6 +30,6 @@ res = Resource(env, false)
 Process(env, generator, res)
 run(env, 2.0)
 env2 = Environment(2.0)
-res2 = Resource(env2, 2)
+res2 = Resource(env2, 2, true)
 Process(env2, generator, res2)
 run(env2, 8.0)
