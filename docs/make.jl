63e2c34bc9cd9b68b0b8ea8d9d6d34af9c924b8a
Ben Lauwens <ben.lauwens@gmail.com>
2016-08-11 13:45:27 +0200

    Another doc commit

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,10 @@ using Documenter
 using SimJulia
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/BenLauwens/SimJulia.jl.git",
+    julia  = "0.5",
+    osname = "linux"
+)
