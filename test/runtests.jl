55f02d0f920bf1dbf046c4cc9eb3579aa38d7d3a
Ben Lauwens <ben.lauwens@gmail.com>
2016-10-06 12:34:31 +0200

    Add tests for operators

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ testpath(f) = joinpath(dirname(@__FILE__), f)
 for test_file in [
   "test_base.jl",
   "test_events.jl",
+  "test_operators.jl",
   "test_simulation.jl",
   "test_process.jl",]
   include(testpath(test_file))
