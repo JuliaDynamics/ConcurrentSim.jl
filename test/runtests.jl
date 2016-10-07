f1a97b276bfb68bb2f440510e986bcb33c7b89a3
Ben Lauwens <ben.lauwens@gmail.com>
2016-10-07 14:07:48 +0200

    add test_cintainers to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ for test_file in [
   "test_events.jl",
   "test_operators.jl",
   "test_simulation.jl",
-  "test_process.jl",]
+  "test_process.jl",
+  "test_containers.jl",]
   include(testpath(test_file))
 end
