a44ea066365e0e31e5fc845d19544e8a74ab5d71
autodocs <autodocs>
2017-09-14 10:41:05 +0000

    build based on 37408cc

--- latest/search_index.js
+++ latest/search_index.js
@@ -2,18 +2,34 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "index.html#",
-    "page": "Home",
-    "title": "Home",
+    "page": "Overview",
+    "title": "Overview",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "index.html#Overview-1",
-    "page": "Home",
+    "page": "Overview",
     "title": "Overview",
     "category": "section",
-    "text": "SimJulia is a discrete event process oriented simulation framework written in Julia inspired by the Python library SimPy."
+    "text": "SimJulia is a discrete-event process-oriented simulation framework written in Julia inspired by the Python library SimPy. Its preferred process dispatcher is based on semi-coroutines scheduling as implemented in ResumableFunctions. A Process in SimJulia is defined by a @resumable function yielding Events. SimJulia also provides three types of shared resources to model limited capacity congestion points: Resources, Containers and Stores. The API is modeled after the SimPy API but using some specific Julia semantics."
+},
+
+{
+    "location": "manual.html#",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual.html#Manual-1",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "section",
+    "text": ""
 },
 
 ]}
