4dcb9a2158ddd693872c264b185ada9229c681db
Johan Van Kerckhoven <johan.vankerckhoven@gmail.com>
2020-09-18 10:30:22 +0200

    Bugfix in Containers
        Containers with Complex as the type of their capacity gave errors
    with put/get requests because z1 < z2 is normally not defined for
    complex numbers.
    Solution: Containers have been restricted to subtypes of Real instead
    of Number.

--- src/resources/containers.jl
+++ src/resources/containers.jl
@@ -1,28 +1,28 @@
-struct ContainerKey{N<:Number} <: ResourceKey
+struct ContainerKey{N<:Real} <: ResourceKey
   priority :: Int
   id :: UInt
   amount :: N
 end
 
-mutable struct Container{N<:Number} <: AbstractResource
+mutable struct Container{N<:Real} <: AbstractResource
   env :: Environment
   capacity :: N
   level :: N
   seid :: UInt
   put_queue :: DataStructures.PriorityQueue{Put, ContainerKey{N}}
   get_queue :: DataStructures.PriorityQueue{Get, ContainerKey{N}}
-  function Container{N}(env::Environment, capacity::N=one(N); level::N=zero(N)) where {N<:Number}
+  function Container{N}(env::Environment, capacity::N=one(N); level::N=zero(N)) where {N<:Real}
     new(env, capacity, level, zero(UInt), DataStructures.PriorityQueue{Put, ContainerKey{N}}(), DataStructures.PriorityQueue{Get, ContainerKey{N}}())
   end
 end
 
-function Container(env::Environment, capacity::N=one(N); level::N=zero(N)) where N<:Number
+function Container(env::Environment, capacity::N=one(N); level::N=zero(N)) where N<:Real
   Container{N}(env, capacity, level=level)
 end
 
 const Resource = Container{Int}
 
-function put(con::Container{N}, amount::N; priority::Int=0) where N<:Number
+function put(con::Container{N}, amount::N; priority::Int=0) where N<:Real
   put_ev = Put(con.env)
   con.put_queue[put_ev] = ContainerKey(priority, con.seid+=one(UInt), amount)
   @callback trigger_get(put_ev, con)
@@ -32,7 +32,7 @@ end
 
 request(res::Resource; priority::Int=0) = put(res, 1; priority=priority)
 
-function get(con::Container{N}, amount::N; priority::Int=0) where N<:Number
+function get(con::Container{N}, amount::N; priority::Int=0) where N<:Real
   get_ev = Get(con.env)
   con.get_queue[get_ev] = ContainerKey(priority, con.seid+=one(UInt), amount)
   @callback trigger_put(get_ev, con)
@@ -42,14 +42,14 @@ end
 
 release(res::Resource; priority::Int=0) = get(res, 1; priority=priority)
 
-function do_put(con::Container{N}, put_ev::Put, key::ContainerKey{N}) where N<:Number
+function do_put(con::Container{N}, put_ev::Put, key::ContainerKey{N}) where N<:Real
   con.level + key.amount > con.capacity && return false
   schedule(put_ev)
   con.level += key.amount
   true
 end
 
-function do_get(con::Container{N}, get_ev::Get, key::ContainerKey{N}) where N<:Number
+function do_get(con::Container{N}, get_ev::Get, key::ContainerKey{N}) where N<:Real
   con.level - key.amount < zero(N) && return false
   schedule(get_ev)
   con.level -= key.amount
