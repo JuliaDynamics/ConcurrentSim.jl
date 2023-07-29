baa80b475f0b3dc814ebe371740ea8322afd992c
Hector Perez <hperez16@gmail.com>
2023-07-28 22:33:19 -0400

    allow priorities to be Number in Containers and Stores. TODO: should make order of parameters consistent in both resource types (Containers has it as the first parameter. Stores has it as the second).

--- src/resources/containers.jl
+++ src/resources/containers.jl
@@ -4,25 +4,25 @@ struct ContainerKey{T<:Number, N<:Real} <: ResourceKey
   amount :: N
 end
 
-mutable struct Container{N<:Real} <: AbstractResource
+mutable struct Container{T<:Number, N<:Real} <: AbstractResource
   env :: Environment
   capacity :: N
   level :: N
   seid :: UInt
-  put_queue :: DataStructures.PriorityQueue{Put, ContainerKey{N}}
-  get_queue :: DataStructures.PriorityQueue{Get, ContainerKey{N}}
-  function Container{N}(env::Environment, capacity::N=one(N); level::N=zero(N)) where {N<:Real}
-    new(env, capacity, level, zero(UInt), DataStructures.PriorityQueue{Put, ContainerKey{N}}(), DataStructures.PriorityQueue{Get, ContainerKey{N}}())
+  put_queue :: DataStructures.PriorityQueue{Put, ContainerKey{T, N}}
+  get_queue :: DataStructures.PriorityQueue{Get, ContainerKey{T, N}}
+  function Container{T, N}(env::Environment, capacity::N=one(N); level::N=zero(N)) where {T<:Number, N<:Real}
+    new(env, capacity, level, zero(UInt), DataStructures.PriorityQueue{Put, ContainerKey{T, N}}(), DataStructures.PriorityQueue{Get, ContainerKey{T, N}}())
   end
 end
 
 function Container(env::Environment, capacity::N=one(N); level::N=zero(N)) where {N<:Real}
-  Container{N}(env, capacity, level=level)
+  Container{Int, N}(env, capacity, level=level)
 end
 
-const Resource = Container{Int}
+const Resource = Container{Int, Int}
 
-function put(con::Container{N}, amount::N; priority::Number=0) where N<:Real
+function put(con::Container{T, N}, amount::N; priority::Number=0) where {T<:Number, N<:Real}
   put_ev = Put(con.env)
   con.put_queue[put_ev] = ContainerKey(priority, con.seid+=one(UInt), amount)
   @callback trigger_get(put_ev, con)
@@ -32,7 +32,7 @@ end
 
 request(res::Resource; priority::Number=0) = put(res, 1; priority=priority)
 
-function get(con::Container{N}, amount::N; priority::Number=0) where N<:Real
+function get(con::Container{T, N}, amount::N; priority::T=0) where {T<:Number, N<:Real}
   get_ev = Get(con.env)
   con.get_queue[get_ev] = ContainerKey(priority, con.seid+=one(UInt), amount)
   @callback trigger_put(get_ev, con)
@@ -42,14 +42,14 @@ end
 
 release(res::Resource; priority::Number=0) = get(res, 1; priority=priority)
 
-function do_put(con::Container{N}, put_ev::Put, key::ContainerKey{N}) where N<:Real
+function do_put(con::Container{T, N}, put_ev::Put, key::ContainerKey{T, N}) where {T<:Number, N<:Real}
   con.level + key.amount > con.capacity && return false
   schedule(put_ev)
   con.level += key.amount
   true
 end
 
-function do_get(con::Container{N}, get_ev::Get, key::ContainerKey{N}) where N<:Real
+function do_get(con::Container{T, N}, get_ev::Get, key::ContainerKey{T, N}) where {T<:Number, N<:Real}
   con.level - key.amount < zero(N) && return false
   schedule(get_ev)
   con.level -= key.amount
--- src/resources/stores.jl
+++ src/resources/stores.jl
@@ -10,22 +10,26 @@ struct StoreGetKey{N<:Number} <: ResourceKey
   filter :: Function
 end
 
-mutable struct Store{T} <: AbstractResource
+mutable struct Store{T, N<:Number} <: AbstractResource
   env :: Environment
   capacity :: UInt
   load :: UInt
   items :: Dict{T, UInt}
   seid :: UInt
-  put_queue :: DataStructures.PriorityQueue{Put, StorePutKey{T}}
-  get_queue :: DataStructures.PriorityQueue{Get, StoreGetKey}
-  function Store{T}(env::Environment; capacity::UInt=typemax(UInt)) where {T}
-    new(env, capacity, zero(UInt), Dict{T, UInt}(), zero(UInt), DataStructures.PriorityQueue{Put, StorePutKey{T}}(), DataStructures.PriorityQueue{Get, StoreGetKey}())
+  put_queue :: DataStructures.PriorityQueue{Put, StorePutKey{N, T}}
+  get_queue :: DataStructures.PriorityQueue{Get, StoreGetKey{N}}
+  function Store{T,N}(env::Environment; capacity::UInt=typemax(UInt)) where {T, N<:Number}
+    new(env, capacity, zero(UInt), Dict{T, UInt}(), zero(UInt), DataStructures.PriorityQueue{Put, StorePutKey{N, T}}(), DataStructures.PriorityQueue{Get, StoreGetKey{N}}())
   end
 end
 
-function put(sto::Store{T}, item::T; priority::Number=0) where T
+function Store{T}(env::Environment; capacity::UInt=typemax(UInt)) where {T}
+  Store{T,Int}(env; capacity)
+end
+
+function put(sto::Store{T, N}, item::T; priority::N=zero(N)) where {T, N<:Number}
   put_ev = Put(sto.env)
-  sto.put_queue[put_ev] = StorePutKey{T}(priority, sto.seid+=one(UInt), item)
+  sto.put_queue[put_ev] = StorePutKey{N, T}(priority, sto.seid+=one(UInt), item)
   @callback trigger_get(put_ev, sto)
   trigger_put(put_ev, sto)
   put_ev
@@ -33,7 +37,7 @@ end
 
 get_any_item(::T) where T = true
 
-function get(sto::Store{T}, filter::Function=get_any_item; priority::Number=0) where T
+function get(sto::Store{T, N}, filter::Function=get_any_item; priority::N=zero(N)) where {T, N<:Number}
   get_ev = Get(sto.env)
   sto.get_queue[get_ev] = StoreGetKey(priority, sto.seid+=one(UInt), filter)
   @callback trigger_put(get_ev, sto)
@@ -41,7 +45,7 @@ function get(sto::Store{T}, filter::Function=get_any_item; priority::Number=0) w
   get_ev
 end
 
-function do_put(sto::Store{T}, put_ev::Put, key::StorePutKey{T}) where {T}
+function do_put(sto::Store{T, N}, put_ev::Put, key::StorePutKey{N,T}) where {T, N<:Number}
   if sto.load < sto.capacity
     sto.load += one(UInt)
     sto.items[key.item] = get(sto.items, key.item, zero(UInt)) + one(UInt)
@@ -50,7 +54,7 @@ function do_put(sto::Store{T}, put_ev::Put, key::StorePutKey{T}) where {T}
   false
 end
 
-function do_get(sto::Store{T}, get_ev::Get, key::StoreGetKey) where {T}
+function do_get(sto::Store{T, N}, get_ev::Get, key::StoreGetKey{N}) where {T, N<:Number}
   for (item, number) in sto.items
     if key.filter(item)
       sto.load -= one(UInt)
