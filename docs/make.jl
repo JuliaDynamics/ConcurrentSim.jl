60963caf8b094dcaefa500efeb9166b95e6cd640
Stefan Krastanov <stefan@krastanov.org>
2023-07-28 20:07:06 -0400

    documentation fixes

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,13 @@ using Documenter
 using ResumableFunctions
 using ConcurrentSim
 
+DocMeta.setdocmeta!(ConcurrentSim, :DocTestSetup, :(using ConcurrentSim, ResumableFunctions); recursive=true)
+
 makedocs(
   sitename = "ConcurrentSim",
   authors = "Ben Lauwens and SimJulia & ConcurrentSim contributors",
+  strict = true,
+  doctest = false,
   pages    = [
     "Home" => "index.md",
     "Tutorial" => "tutorial.md",
