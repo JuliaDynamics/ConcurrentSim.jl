dc12196c2ffe2f15b5ab93bde8c0c6859269deed
Stefan Krastanov <github.acc@krastanov.org>
2024-09-12 09:23:12 -0400

    simplify jet tests (#128)

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -2,32 +2,15 @@ using ConcurrentSim
 using JET
 using Test
 
-using JET: ReportPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport
-
 using InteractiveUtils
-
-# Custom report pass that ignores `UncaughtExceptionReport`
-# Too coarse currently, but it serves to ignore the various
-# "may throw" messages for runtime errors we raise on purpose
-# (mostly on malformed user input)
-struct MayThrowIsOk <: ReportPass end
-
-# ignores `UncaughtExceptionReport` analyzed by `JETAnalyzer`
-(::MayThrowIsOk)(::Type{UncaughtExceptionReport}, @nospecialize(_...)) = return
-
-# forward to `BasicPass` for everything else
-function (::MayThrowIsOk)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    BasicPass()(report_type, args...)
-end
 
 @testset "JET checks" begin
     rep = report_package("ConcurrentSim";
-        report_pass=MayThrowIsOk(),
         ignored_modules=(
             AnyFrameModule(InteractiveUtils),
         )
     )
     @show rep
     @test_broken length(JET.get_reports(rep)) == 0
-    @test length(JET.get_reports(rep)) <= 4
+    @test length(JET.get_reports(rep)) <= 3
 end
