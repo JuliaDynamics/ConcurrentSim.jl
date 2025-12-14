c5e909f1e0b228427d3a4165150484523e91d31f
Stefan Krastanov <stefan@krastanov.org>
2025-12-14 03:37:36 +0000

    docs: enable AnythingLLMDocs chat

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,28 @@
 using Revise
 using Documenter
+using AnythingLLMDocs
 using ResumableFunctions
 using ConcurrentSim
 
 DocMeta.setdocmeta!(ConcurrentSim, :DocTestSetup, :(using ConcurrentSim, ResumableFunctions); recursive=true)
 
+doc_modules = [ConcurrentSim]
+
+api_base="https://anythingllm.krastanov.org/api/v1"
+anythingllm_assets = integrate_anythingllm(
+  "ConcurrentSim",
+  doc_modules,
+  @__DIR__,
+  api_base;
+  repo = "github.com/JuliaDynamics/ConcurrentSim.jl.git",
+  options = EmbedOptions(),
+)
+
 makedocs(
   sitename = "ConcurrentSim",
   authors = "Ben Lauwens and SimJulia & ConcurrentSim contributors",
   doctest = false,
+  format = Documenter.HTML(assets = anythingllm_assets),
   pages    = [
     "Home" => "index.md",
     "Tutorial" => "tutorial.md",
